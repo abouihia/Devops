cc100a8ea52c56ea8a6a7927e6f438b0242860d1
brahim abouihia <abouihia.brahim@gmail.com>
2020-05-05 16:05:46 +0200

    first commit devops

---
+++ script.groovy
@@ -0,0 +1,19 @@
+
+node {
+    try{
+          stage('pull'){
+              sh 'echo brahim BodyAttak'
+          }
+           stage('compile'){
+              sh 'echo brahim BodyAttak'
+          }
+          stage('Test'){
+              sh 'echo brahim BodyAttak'
+          }
+           stage('Sonar'){
+              sh 'echo brahim BodyAttak'
+          }
+    }finally{
+       cleanWs() 
+    }
+}
