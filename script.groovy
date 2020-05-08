e80f6b1b18614b270af2294c01b5294fbbcd3bf3
abouihia <abouihia.brahim@gmail.com>
2020-05-08 12:39:19 +0200

    suppression properties  liées au process GitLab
    Recherche la façon de faire sur gitHub

--- script.groovy
+++ script.groovy
@@ -1,31 +1,5 @@
 def project_token = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEF'
 
-properties([
-    gitLabConnection('your-gitlab-connection-name'),
-    pipelineTriggers([
-        [
-            $class: 'GitLabPushTrigger',
-            branchFilterType: 'All',
-            triggerOnPush: true,
-            triggerOnMergeRequest: true,
-            triggerOpenMergeRequestOnPush: "never",
-            triggerOnNoteRequest: true,
-            noteRegex: "Jenkins please retry a build",
-            skipWorkInProgressMergeRequest: true,
-            secretToken: project_token,
-            ciSkip: false,
-            setBuildDescription: true,
-            addNoteOnMergeRequest: true,
-            addCiMessage: true,
-            addVoteOnMergeRequest: true,
-            acceptMergeRequestOnSuccess: true,
-            branchFilterType: "NameBasedFilter",
-            includeBranchesSpec: "",
-            excludeBranchesSpec: "",
-        ]
-    ])
-])
-
 
 node(){
   try{
